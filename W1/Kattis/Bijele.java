08f7170eacde8ab015f2765c4f9e4610cfcd482e
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-10-26 15:28:35 +0200

    add package line

--- W1/Kattis/Bijele.java
+++ W1/Kattis/Bijele.java
@@ -1,19 +1,21 @@
+package Kattis;
+
 import java.util.Scanner;
 
 public class Bijele {
 
 	public static void main(String[] args) {
-		 Scanner sc = new Scanner(System.in);
-	        int pieces[]= {1,1,2,2,2,8};
-	        int arr[]=new int[6];
-	        int i;
-	        int sum=0;
-	        for(i=0;i<6;i++) {
-	        arr[i]=sc.nextInt();
-	        sum=pieces[i]-arr[i];
-	        System.out.print(sum+" ");
-	        sum=0;
-	        }
+		Scanner sc = new Scanner(System.in);
+		int pieces[] = { 1, 1, 2, 2, 2, 8 };
+		int arr[] = new int[6];
+		int i;
+		int sum = 0;
+		for (i = 0; i < 6; i++) {
+			arr[i] = sc.nextInt();
+			sum = pieces[i] - arr[i];
+			System.out.print(sum + " ");
+			sum = 0;
+		}
 	}
 
 }
--- W1/Kattis/cold.java
+++ W1/Kattis/cold.java
@@ -1,14 +1,17 @@
+package Kattis;
+
 import java.util.Scanner;
+
 public class cold {
 
 	public static void main(String[] args) {
-		Scanner scanner= new Scanner(System.in);
-		int n=scanner.nextInt();
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
 		int zahl;
-		int t=0;
-		for(int i=0;i<n;i++) {
-			zahl=scanner.nextInt();
-			if(zahl<0) {
+		int t = 0;
+		for (int i = 0; i < n; i++) {
+			zahl = scanner.nextInt();
+			if (zahl < 0) {
 				t++;
 			}
 		}
