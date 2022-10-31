9477f44cf0c5a50aec82e6dc742d0b492f51232e
HoangKim1504 <101057915+HoangKim1504@users.noreply.github.com>
2022-11-01 06:14:32 +0700

    Create nastyhacks.java

---
+++ W2/Kattis/nastyhacks.java
@@ -0,0 +1,25 @@
+package W2.Kattis;
+
+import java.util.Scanner;
+
+public class NastyHacks {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		int n = sc.nextInt();
+		
+		for(int i = 0; i < n; i++) {
+			int r = sc.nextInt();
+			int e = sc.nextInt();
+			int c = sc.nextInt();
+			
+			if(e - c > r) {
+				System.out.println("advertise");
+			} else if (e - c < r) {
+				System.out.println("do not advertise");
+			} else {
+				System.out.println("does not matter");
+			}
+		}		
+	}
+}
