1c7b9d4e20d06272bd87d4e4b746a538e0d4fb24
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-09 10:10:18 +0100

    Add files via upload

---
+++ W3/Kattis/boatpart.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class boatpart {
+
+	public static void main(String[] args) {
+		int i=0;
+		  Scanner enteringvalues = new Scanner(System.in);
+		  String[] line = enteringvalues.nextLine().split(" ");
+		    int parts = Integer.parseInt(line[0]);
+		    int days = Integer.parseInt(line[1]);
+		    HashSet<String> hashset = new HashSet<>();
+		    do{
+		      String currentline = enteringvalues.nextLine();
+		      hashset.add(currentline);
+		      if (hashset.size() == parts) {
+		        System.out.println(i + 1);
+		        break;
+		      }
+		      if(i== days-1){
+		        System.out.println("paradox avoided");
+		      }
+		      i++;
+		    }while(i<days);
+
+	}
+
+}
