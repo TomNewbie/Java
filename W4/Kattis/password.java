b3ee56b63ef51c355c41697fe63197a942251392
Tho PC <48612315+TomNewbie@users.noreply.github.com>
2022-11-17 15:49:44 +0100

    move file to kattis folder

---
+++ W4/Kattis/password.java
@@ -0,0 +1,28 @@
+package Kattis;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+// package W4;
+
+public class password {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int testCase = scan.nextInt();
+        ArrayList<Float> probability = new ArrayList<>();
+        for (int i = 0; i < testCase; i++) {
+            String password = scan.next();
+            probability.add(scan.nextFloat());
+        }
+        Collections.sort(probability);
+        int count = probability.size();
+        float result = 0;
+        for (float probs : probability) {
+            result = result + count * probs;
+            count--;
+        }
+        System.out.println(result);
+        scan.close();
+    }
+}
---
+++ W4/Kattis/whatdoesthefoxsay.java
@@ -0,0 +1,39 @@
+package Kattis;
+// package W4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class whatdoesthefoxsay {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numTest = scan.nextInt();
+        scan.nextLine();
+        String terminate = new String("what does the fox say?");
+        for (int i = 0; i < numTest; i++) {
+            String record = scan.nextLine();
+            String[] soundArr = record.split(" ");
+
+            ArrayList<String> soundArrList = new ArrayList<>(Arrays.asList(soundArr));
+            String animal = scan.nextLine();
+            ArrayList<String> animalSound = new ArrayList<>();
+            while (!animal.equals(terminate)) {
+                String[] splitAnimal = animal.split(" ");
+                String sound = splitAnimal[splitAnimal.length - 1];
+                animalSound.add(sound);
+                animal = scan.nextLine();
+            }
+            // delete other sound in soundArr
+            for (String noise : animalSound) {
+                while (soundArrList.remove(noise))
+                    ;
+            }
+            for (String fox : soundArrList) {
+                System.out.print(fox + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
