ced7ef531b950fad27b7e378fc6377ef6337d630
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-17 19:54:09 +0100

    Create RandomSeqLotto.java

---
+++ W1/T4/RandomSeqLotto.java
@@ -0,0 +1,12 @@
+public class RandomSeqLotto {
+
+	public static void main(String[] args) {
+	    int i;
+	    for(i=0;i<6;i++) {
+	    	int num=(int)(Math.random()*49)+1;
+	    	System.out.print(num+ " ");
+	    }
+
+	}
+
+}
