8a5715a51db2d59abe4f5f699670d67e54b4a5c8
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-17 19:37:51 +0100

    Update Quickestimates.java

--- W2/Kattis/Quickestimates.java
+++ W2/Kattis/Quickestimates.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Quickestimates {
 
@@ -6,10 +7,14 @@ public class Quickestimates {
 		int n=Integer.parseInt(enter.nextLine());
 		int j;
 		int mod=0;
+		ArrayList<String> al=new ArrayList<String>();
 		for(j=0;j<n;j++) {
 			String s= enter.nextLine();
-		System.out.println(s.length());
+			al.add(s);
+		    
 		}
+		for(j=0;j<n;j++) {
+		System.out.println(al.get(j).length());}
 	}
 
 }
