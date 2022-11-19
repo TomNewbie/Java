cdcf60e16854786c1544da7d30fd47b6406ec556
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-19 11:29:31 +0100

    Create parking.java

---
+++ W2/Kattis/parking/parking.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class parking {
+	
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+    int A, B, C, g, h,j,i;int l=0;
+    int[] mynumbers = {0, read.nextInt(), read.nextInt(), read.nextInt()};
+    
+    A = read.nextInt();
+    B = read.nextInt();
+    C = read.nextInt();
+    g = read.nextInt();
+    h = read.nextInt();
+    i = read.nextInt();
+    int currentnum = 0;
+    int num = 0;
+    for (j = 1; j < 100; j++) {
+      if(j == A) {
+        currentnum++;}
+      if(j == B) {
+          currentnum++;}
+      if(j ==  C) {
+        currentnum++;}
+      if(j ==  h) {
+        currentnum++;}
+      if(j == g) {
+        currentnum--;}
+      if(j == i) {
+        currentnum--;}
+      num+=currentnum * mynumbers[currentnum];
+    }
+    System.out.println(num);
+  }
+}
