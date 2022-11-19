859751ef2bd826c79ff52d20aa18973a45fd7f6a
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-19 11:42:21 +0100

    Delete realChance.java

--- W3/Kattis/realChance.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class realChance {
-	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		long a=sc.nextLong();
-		 System.out.println(Math.sqrt(a)*4);
-
-}
-}
