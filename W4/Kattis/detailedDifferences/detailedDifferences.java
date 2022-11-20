707ad05fd4fbb27734d2c75d3434bd54a810dbd0
HoangKim1504 <101057915+HoangKim1504@users.noreply.github.com>
2022-11-21 06:30:56 +0700

    Add comment and create folder

--- W4/Kattis/detailedDifferences/detailedDifferences.java
+++ W4/Kattis/detailedDifferences/detailedDifferences.java
@@ -1,3 +1,15 @@
+/**
+* Advanced Object Oriented Programming with Java, WS 2022
+* Problem: Detailed Differences
+* Link: https://open.kattis.com/contests/tbnznz/problems/detaileddifferences
+* @author Trần Hoàng Kim 
+* @version 1.0, 2022-11-18
+*
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.18s 
+*/
+
 import java.util.Scanner;
 
 public class detailedDifferences {
