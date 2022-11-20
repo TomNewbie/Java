d8a433a7dba1c757f45f973234d040392664a54d
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-20 15:52:02 +0100

    Update realChance.java

--- W3/Kattis/aRealchance/realChance.java
+++ W3/Kattis/aRealchance/realChance.java
@@ -1,4 +1,15 @@
 import java.util.Scanner;
+/**
+* Advanced Object Oriented Programming with Java, WS 2022
+* Problem: Problem A Real Challenge
+* Link: https://open.kattis.com/contests/uk27ry/problems/areal
+* @author Ferid Gökkaya
+* @version 1.0, 2022-11-06 
+* 
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.102
+*/
 public class realChance {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
