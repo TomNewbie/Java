59be786552a0fd9aa23eeee2b859306626d85bc3
HoangKim1504 <101057915+HoangKim1504@users.noreply.github.com>
2022-11-21 04:55:12 +0700

    Update IsItHalloween.java

--- W2/Kattis/IsItHalloween.java
+++ W2/Kattis/IsItHalloween.java
@@ -1,6 +1,17 @@
-package W2.Kattis;
+/**
+* Advanced Object Oriented Programming with Java, WS 2022
+* Problem: IsItHalloween.com
+* Link: https://open.kattis.com/contests/ggi5da/problems/isithalloween
+* @author Trần Hoàng Kim
+* @version 1.0, 2022-11-01 
+* 
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.10s
+*/
 
 import java.util.Scanner;
+
 public class IsItHalloween {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
