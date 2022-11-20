b8f620eb939e78c947401a7a68ce7dfd39bcfbc3
Ferid Gökkaya <116716082+ferid99@users.noreply.github.com>
2022-11-20 15:57:08 +0100

    Update unbearablezoo.java

--- W4/Kattis/unbearablezoo/unbearablezoo.java
+++ W4/Kattis/unbearablezoo/unbearablezoo.java
@@ -1,38 +1,49 @@
 import java.util.*;
+/**
+* Advanced Object Oriented Programming with Java, WS 2022
+* Problem: Problem A Un-bear-able Zoo
+* Link: https://open.kattis.com/contests/tbnznz/problems/zoo
+* @author Ferid Gökkaya
+* @version 1.0, 2022-11-14 
+* 
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.102
+*/
+import java.util.*;
 public class unbearablezoo {
-     public static void show(ArrayList<String> mylist,HashMap<String,Integer>myAnimals) {
-    	 for(String foo:mylist) {
- 			System.out.println(foo+" | "+myAnimals.get(foo));
- 		}
-     }
-     
-	public static void main(String[] args) {
-		Scanner read = new Scanner(System.in);
-		HashMap<String,Integer> myAnimals=new HashMap<String,Integer>();
-		ArrayList<String> mylist=new ArrayList<String>();
-		int num=0,j;
-		int num2=0,num3=0;
-		while(true) {
-			 num2=Integer.parseInt(read.nextLine());
-			if(num2==0) {
-				return;
-			}
-		for(j=0;j<num2;j=j+1) {
-			String animal[]=read.nextLine().split(" ");
-		   int length=animal.length;
-			animal[length-1]=animal[length-1].toLowerCase();
-			if(!myAnimals.containsKey(animal[length-1])) {
-				myAnimals.put(animal[length-1],1);
-				mylist.add(animal[length-1]);
-			}else {
-				num3 = myAnimals.get(animal[length-1]);
-			myAnimals.replace(animal[length-1], num3+1);
-		}
-		}
-		
-		System.out.println("List " + ++num+":");
-		Collections.sort(mylist);
-		
-		show(mylist,myAnimals);
-		}}}
+
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        int num=0,j;
+        int num2=0,num3=0;
+        while(true) {
+             num2=Integer.parseInt(read.nextLine());
+            if(num2==0) {
+                return;
+            }
+            HashMap<String,Integer> myAnimals=new HashMap<String,Integer>();
+            ArrayList<String> mylist=new ArrayList<String>();
+        for(j=0;j<num2;j=j+1) {
+            String animal[]=read.nextLine().split(" ");
+           int length=animal.length;
+            animal[length-1]=animal[length-1].toLowerCase();
+            if(!myAnimals.containsKey(animal[length-1])) {
+                myAnimals.put(animal[length-1],1);
+                mylist.add(animal[length-1]);
+            }else {
+                num3 = myAnimals.get(animal[length-1]);
+            myAnimals.replace(animal[length-1], num3+1);
+        }
+        }
+        
+        System.out.println("List " + ++num+":");
+        
+    
+        Collections.sort(mylist);
+        for(String foo:mylist) {
+            System.out.println(foo+" | "+myAnimals.get(foo));
+        }
+        }}}
+
 
